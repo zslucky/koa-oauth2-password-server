020f48820f8f569361a91a4f83dc46946562b7ba
lucky ZHOU <lucky.zhou@xplusz.com>
2016-08-10 13:10:53 +0800

    Add cors

---
+++ src/configs/cors.config.js
@@ -0,0 +1,5 @@
+const corsOptions = {
+  headers: 'Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin',
+};
+
+module.exports = corsOptions;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const koa = require('koa');
 const argv = require('argv');
+const cors = require('koa-cors');
 const mount = require('koa-mount');
 const Router = require('koa-router');
 const logger = require('koa-logger');
 const bodyparser = require('koa-bodyparser');
 const oauthserver = require('koa-oauth-server');
 const model = require('./model');
+const corsOptions = require('./configs/cors.config');
 const argvOptions = require('./configs/argv.config');
 
 const app = koa();
@@ -14,6 +16,8 @@ const router = new Router();
 const args = argv.option(argvOptions).run();
 const port = args.options.port || 3000;
 
+app.use(cors(corsOptions));
+app.use(logger());
 app.use(bodyparser());
 
 app.oauth = oauthserver({
@@ -21,8 +25,6 @@ app.oauth = oauthserver({
   grants: ['password', 'refresh_token'],
   debug: true,
 });
-
-app.use(logger());
 
 /*
  *  Secret router here.
